f9c0bee99aa9590deb3ff2821108c50e335261a4
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-07 15:40:28 -0800

    added fetch instructions

---
+++ weather.js
@@ -0,0 +1,16 @@
+
+const URL =  "https://community-open-weather-map.p.rapidapi.com/weather?q=Los%20Angeles%2C%20USA&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml";
+var key = config.MY_API_KEY;
+fetch(URL, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": 
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
