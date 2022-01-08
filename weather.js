20791000dd66ec40a602631432ec6892646b91d7
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-07 16:58:07 -0800

    added HTTPS to URL

--- weather.js
+++ weather.js
@@ -1,17 +1,12 @@
 
 var key = config.KEY_SECOND;
-const URL =  "api.openweathermap.org/data/2.5/weather?lat=34.052235&lon=-118.243683&appid=" + key + "";
+const URL =  "https://api.openweathermap.org/data/2.5/weather?lat=34.052235&lon=-118.243683&appid=" + key ;
 
-fetch(URL, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": key 
-	}
-})
-.then(response => {
-	console.log(response.body);
-})
-.catch(err => {
-	console.error(err);
-});
+fetch(URL)
+	.then(response => {
+		return response.json();
+	})
+	.then(response => {
+		console.log(response);
+	})
+
