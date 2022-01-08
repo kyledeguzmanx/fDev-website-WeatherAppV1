6fdfcf6a41ce36f3169841076925a581757d0b9f
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-07 23:07:10 -0800

    new video background

--- weather.js
+++ weather.js
@@ -1,7 +1,12 @@
+let video = document.getElementById("videoBG");
+
 
 var key = config.KEY_SECOND;
 var [latitude, longitude] = [34.052235, -118.243683];
 const URL =  "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude +"&lon=" + longitude + "&appid=" + key ;
+
+video.src= "img/morning.mp4";
+
 
 fetch(URL)
 	.then(response => {
