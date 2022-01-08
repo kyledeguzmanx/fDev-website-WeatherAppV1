ffaa7479dedb55f187962bc1d6a0bef8658e5d21
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-07 23:28:32 -0800

    clear

--- weather.js
+++ weather.js
@@ -5,7 +5,9 @@ var key = config.KEY_SECOND;
 var [latitude, longitude] = [34.052235, -118.243683];
 const URL =  "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude +"&lon=" + longitude + "&appid=" + key ;
 
-video.src= "img/morning.mp4";
+
+
+
 
 
 fetch(URL)
